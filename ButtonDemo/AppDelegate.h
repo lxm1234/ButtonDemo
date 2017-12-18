c2b2adb4894da954e584d6781204b8efc823e746
lxm <1027381389@qq.com>
2017-12-19 00:24:50 +0800

    Initial Commit

---
+++ ButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonDemo
+//
+//  Created by lxm on 2017/12/19.
+//  Copyright © 2017年 lixiaomeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
